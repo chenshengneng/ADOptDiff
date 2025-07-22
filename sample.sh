b75bb747e60d63023617ce6e8cf3b946221e6387
chenshengneng <20185398@cqu.edu.cn>
2025-07-22 16:30:58 +0800

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=2 python -W ignore sample.py \
                  --checkpoint ckpt/try.ckpt\
                  --samples sample_mols \
-                 --data /public/home/chensn/DL/DiffDec-master/data/data \
+                 --data /public/home/chensn/DL/ADOptDiff/data/data \
                  --prefix bingdingnet_test_full \
                  --n_samples 100 \
                  --device cuda:0
